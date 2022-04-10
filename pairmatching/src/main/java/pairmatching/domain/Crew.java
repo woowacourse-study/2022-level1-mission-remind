ebfd601d51558c639433d6832e44156ba75bcc65
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-04-10 17:00:42 +0900

    feat: 코스 추가 및 크루원 코스 null인 경우 예외발생 테스트

--- pairmatching/src/main/java/pairmatching/domain/Crew.java
+++ pairmatching/src/main/java/pairmatching/domain/Crew.java
@@ -5,11 +5,14 @@ import java.util.Objects;
 public class Crew {
 
     private final String name;
+    private final Course course;
 
-    public Crew(final String name) {
+    public Crew(final String name, final Course course) {
         Objects.requireNonNull(name, "[ERROR] 이름 null불가");
+        Objects.requireNonNull(course, "[ERROR] 코스 null불가");
         validateEmptyName(name);
         this.name = name;
+        this.course = course;
     }
 
     private void validateEmptyName(final String name) {
--- pairmatching/src/test/java/pairmatching/domain/CrewTest.java
+++ pairmatching/src/test/java/pairmatching/domain/CrewTest.java
@@ -1,6 +1,7 @@
 package pairmatching.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static pairmatching.domain.Course.BACKEND;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EmptySource;
@@ -10,17 +11,25 @@ class CrewTest {
 
     @ParameterizedTest
     @NullSource
-    void 이름이_null인_경우_예외발생(final String input) {
-        assertThatThrownBy(() -> new Crew(input))
+    void 이름이_null인_경우_예외발생(final String name) {
+        assertThatThrownBy(() -> new Crew(name, BACKEND))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("[ERROR] 이름 null불가");
     }
 
     @ParameterizedTest
     @EmptySource
-    void 이름이_공백인_경우_예외발생(final String input) {
-        assertThatThrownBy(() -> new Crew(input))
+    void 이름이_공백인_경우_예외발생(final String name) {
+        assertThatThrownBy(() -> new Crew(name, BACKEND))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] 이름 공백불가");
+    }
+
+    @ParameterizedTest
+    @NullSource
+    void 코스가_null인_경우_예외발생(final Course course) {
+        assertThatThrownBy(() -> new Crew("crew", course))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("[ERROR] 코스 null불가");
     }
 }
