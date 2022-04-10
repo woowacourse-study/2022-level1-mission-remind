67fcfb9d5d80e61b18c1ee67b4a4cf7458354d85
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-04-10 16:56:57 +0900

    feat: 크루원 이름이 공백인 경우 예외 발생

--- pairmatching/src/main/java/pairmatching/domain/Crew.java
+++ pairmatching/src/main/java/pairmatching/domain/Crew.java
@@ -8,6 +8,13 @@ public class Crew {
 
     public Crew(final String name) {
         Objects.requireNonNull(name, "[ERROR] 이름 null불가");
+        validateEmptyName(name);
         this.name = name;
+    }
+
+    private void validateEmptyName(final String name) {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("[ERROR] 이름 공백불가");
+        }
     }
 }
--- pairmatching/src/test/java/pairmatching/domain/CrewTest.java
+++ pairmatching/src/test/java/pairmatching/domain/CrewTest.java
@@ -3,6 +3,7 @@ package pairmatching.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 
 class CrewTest {
@@ -13,5 +14,13 @@ class CrewTest {
         assertThatThrownBy(() -> new Crew(input))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("[ERROR] 이름 null불가");
+    }
+
+    @ParameterizedTest
+    @EmptySource
+    void 이름이_공백인_경우_예외발생(final String input) {
+        assertThatThrownBy(() -> new Crew(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 이름 공백불가");
     }
 }
