0d25e63b744e5d6c85338f65ccfab65a8e5cd625
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-04-10 18:25:30 +0900

    feat: Pair 생성 시 정해진 수가 아닌 경우 예외추가 및 테스트

--- pairmatching/src/main/java/pairmatching/domain/Pair.java
+++ pairmatching/src/main/java/pairmatching/domain/Pair.java
@@ -1,15 +1,22 @@
 package pairmatching.domain;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 public class Pair {
 
-    private final List<Crew> crews;
+    private final Set<Crew> crews;
 
-    public Pair(final List<Crew> crews) {
+    public Pair(final Set<Crew> crews) {
         Objects.requireNonNull(crews, "[ERROR] crews null불가");
-        this.crews = new ArrayList<>(crews);
+        this.crews = new LinkedHashSet<>(crews);
+        validatePairSize(crews);
+    }
+
+    private void validatePairSize(final Set<Crew> crews) {
+        if (crews.size() != 2 && crews.size() != 3) {
+            throw new IllegalArgumentException("[ERROR] 페어는 2명 또는 3명만 가능");
+        }
     }
 }
