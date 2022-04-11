4b10fa55d153366faaea483bba4d13dd2d283644
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-04-12 04:50:59 +0900

    feat: PairProgram null 처리 및 테스트

---
+++ pairmatching/src/test/java/pairmatching/domain/PairProgramTest.java
@@ -0,0 +1,18 @@
+package pairmatching.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+
+class PairProgramTest {
+
+    @ParameterizedTest
+    @NullSource
+    void 미션_프로그램_리스트가_null인_경우_예외발생(final List<MissionProgram> missionProgramss) {
+        assertThatThrownBy(() -> new PairProgram(missionProgramss))
+                .isInstanceOf(NullPointerException.class)
+                .hasMessage("[ERROR] missionPrograms null불가");
+    }
+}
