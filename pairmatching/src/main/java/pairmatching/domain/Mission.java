c10181e351a54831280db8c4fec38df8a80e3635
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-04-12 04:16:36 +0900

    feat: Mission 추가 및 이름 null인 경우 예외 추가

--- pairmatching/src/main/java/pairmatching/domain/Mission.java
+++ pairmatching/src/main/java/pairmatching/domain/Mission.java
@@ -1,13 +1,16 @@
 package pairmatching.domain;
 
+import java.util.List;
 import java.util.Objects;
 
 public class Mission {
 
     private final String name;
+    private final List<Pair> pairs;
 
-    public Mission(final String name) {
+    public Mission(final String name, final List<Pair> pairs) {
         Objects.requireNonNull(name, "[ERROR] 미션 이름 null불가");
         this.name = name;
+        this.pairs = pairs;
     }
 }
--- pairmatching/src/test/java/pairmatching/domain/MissionTest.java
+++ pairmatching/src/test/java/pairmatching/domain/MissionTest.java
@@ -2,6 +2,7 @@ package pairmatching.domain;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 
@@ -10,7 +11,7 @@ class MissionTest {
     @ParameterizedTest
     @NullSource
     void 미션_이름이_null인_경우_예외발생(final String name) {
-        assertThatThrownBy(() -> new Mission(name))
+        assertThatThrownBy(() -> new Mission(name, new ArrayList<>()))
                 .isInstanceOf(NullPointerException.class)
                 .hasMessage("[ERROR] 미션 이름 null불가");
     }
